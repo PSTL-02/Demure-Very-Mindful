e0e35551340ea98fc1af0ba3b0cc6c06e3e52557
Miaroaming <mianzroaming@gmail.com>
2024-08-28 11:12:18 +1200

    commentModel completeo

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -1,1 +1,16 @@
-// comment model
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    user_id: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Comment', commentSchema);
