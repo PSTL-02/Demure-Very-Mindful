50aed5b14c107926d3daced5b1a96021fb3877f1
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-08-29 09:55:15 +1200

    added some content and layout to the single project page

--- frontend/src/pages/SingleProject.jsx
+++ frontend/src/pages/SingleProject.jsx
@@ -7,9 +7,36 @@ const SingleWorkout = () => {
 
   return (
     <>
+    <div className='backbtn-title'>
         <button onClick={() => navigate(-1)} className='back-button'> Back </button>
-        <div>
-            <h1> Single Workout Page || Work In Progress <ConeStriped/> </h1>
+        <h2> Project Title </h2>
+    </div>
+        <div className='single-project-content'>
+            <div className='project-image-link'>
+                <img src="" alt="photo of project here" />
+                <a> Link To Portfolio URL </a>
+            </div>
+
+            <div className='project-details'>
+                <h3> Project Type </h3>
+
+                <h4> Project Creators: </h4>
+                <p> Mia, Neel, and Caitlin </p>
+
+                {/* in depth description of project */}
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Sint ratione magni totam deserunt qui iste praesentium. </p>
+
+                <p> Date Uploaded/Created: 20/12/23 </p>
+
+                <h4> Socials: </h4>
+                <div className='social-icons'>
+                    <ConeStriped/>
+                    <ConeStriped/>
+                    <ConeStriped/>
+                </div>
+            </div>
+
         </div>
     </>
   )
