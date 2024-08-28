890cd584e3bfffea17b25fc402fc8ab4e6feefc0
PSTL-02 <tbnrneelplayz@gmail.com>
2024-08-29 09:54:15 +1200

    done with hooks and added the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 import './App.css'
 
-const App = () => {
+// Import Pages
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import SingleWorkout from './pages/SingleProject';
 
+const App =() => {
+  const {user} = useAuthContext();
+  
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar/>
+        <div className='pages'>
+          <Routes>
+            {/* If we have a user show home, else go to login: */}
+            <Route path='/' element={user ? <Home/> : <Navigate to="/login"/>}/>
+            <Route path='/:id' element={user ? <SingleWorkout/> : <Navigate to="/login"/>}/>
+            {/* If we don't have a user show login, if we do have a user got to home  */}
+            <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
+            {/* If we don't have a user show signup, if we do show home */}
+            <Route path='/signup' element={!user ? <Signup/> : <Navigate to="/"/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
